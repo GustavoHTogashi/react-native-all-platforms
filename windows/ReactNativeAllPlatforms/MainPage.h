8797176990bdddfe9359863a10941a6627648d68
Gustavo Togashi <ghtogashi@gmail.com>
2023-12-06 00:48:13 -0300

    feat: windows-uwp

---
+++ windows/ReactNativeAllPlatforms/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ReactNativeAllPlatforms::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ReactNativeAllPlatforms::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
