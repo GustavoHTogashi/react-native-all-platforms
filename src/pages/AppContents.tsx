c169565fe4dad8f851a887d710e68badbc5c8bab
Gustavo Togashi <ghtogashi@gmail.com>
2024-02-06 23:13:16 -0300

    build: update react-native and dependencies

--- src/pages/AppContents.tsx
+++ src/pages/AppContents.tsx
@@ -1,7 +1,23 @@
 // import { Button, ScrollView, Text } from 'tamagui';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { Button, Card, Text, View } from 'tamagui';
 
 export const AppContents = () => {
-	return <SafeAreaView></SafeAreaView>;
+	return (
+		<SafeAreaView style={{ height: '100%', backgroundColor: '#fff' }}>
+			<View padding={16}>
+				<Card>
+					<Card.Header>
+						<Text>Card Header</Text>
+					</Card.Header>
+					<Card.Footer>
+						<Text>Card Footer</Text>
+					</Card.Footer>
+					<Button>Button</Button>
+					<Card.Background />
+				</Card>
+			</View>
+		</SafeAreaView>
+	);
 };
